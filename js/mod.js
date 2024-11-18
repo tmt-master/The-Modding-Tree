74fd43a87097db9ae59be6f8522dada9712fca49
tmt-master <maah6928@rscloud.ca>
2024-11-18 13:21:36 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -3,7 +3,7 @@ let modInfo = {
 	id: "mymo1qd",
 	author: "tmt-master",
 	pointsName: "points",
-	modFiles: ["layers.js", "tree.js"],
+	modFiles: ["p1.js", "p2.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
@@ -43,7 +43,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(0)
-	if (layers.p.effect().gte(1)) gain = gain.add(layers.p.effect())
+	if (layers.p1.effect().gte(1)) gain = gain.add(layers.p1.effect())
 	return gain
 }
 
