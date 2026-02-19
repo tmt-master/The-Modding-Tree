dc50419f27eb4779edfb458429c102618ffb2303
tmt-master <maah6928@rscloud.ca>
2026-02-19 13:06:46 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -49,6 +49,7 @@ function getPointGen() {
 	if (hasUpgrade('p', 21)) gain = gain.times(2.9)
 	if (hasUpgrade('p', 23)) gain = gain.times(upgradeEffect('p', 23))
 	if (hasUpgrade('p', 25)) gain = gain.times(upgradeEffect('p', 25))
+	if (hasUpgrade('p', 33)) gain = gain.times(upgradeEffect('p', 33))
 	return gain
 }
 
