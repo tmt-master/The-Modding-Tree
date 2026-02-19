497f25d2709510bcd9a8cd5403c3ebf9ad42f39e
tmt-master <maah6928@rscloud.ca>
2026-02-19 12:45:12 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -139,7 +139,7 @@ addLayer("p", {
             description: "Divide mastered point cost by (Lansdowne's formula x Princess Anne's formula)^0.5.",
             cost: new Decimal(22),
             unlocked() { return (hasUpgrade('p', 25) },
-			effect() {return player.p.points.div(2.9).add(1).pow(2.9).times(player.p.points.add(1).log(2.9)).times(player.p.points.div(100).add(1).pow(4).times(player.p.points.add(1).log(1.7).pow(2)).add(1)).pow(0.5).add(1)},
+			effect() {return ((player.p.points.div(2.9).add(1).pow(2.9).times(player.p.points.add(1).log(2.9))).times(player.p.points.div(100).add(1).pow(4).times(player.p.points.add(1).log(1.7).pow(2)).add(1)).pow(0.5)).add(1)},
             effectDisplay() {return "x"+format(this.effect())},
 	},
     },
